651fd1e30ded7952e701e51c3dce6c6ab876197c
Joyeeta Sarkar <joyeetasarkar1410@gmail.com>
2023-06-11 19:01:09 +0600

    added middlewares

--- index.js
+++ index.js
@@ -3,7 +3,6 @@ const app=express();
 const cors = require('cors');
 const port = process.env.PORT || 5000;
 
-require('dotenv').config()
 
 app.get('/', (req,res)=>{
     res.send("Sitting on a high speed voltage");
